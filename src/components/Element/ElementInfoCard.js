37f881c25241fa41aab04679743178142cae91e9
SHAN003P <shanfoton33@gmail.com>
2025-04-03 12:08:50 +0530

    Update ElementInfoCard.js

--- src/components/Element/ElementInfoCard.js
+++ src/components/Element/ElementInfoCard.js
@@ -20,7 +20,7 @@ function ElementInfoCard(props) {
                     transition: 'all 0.2s',
                     opacity: hidden ? 0 : 1,
                     transform: `scale(${hidden ? 0.5 : 1})`,
-                    'box-shadow': '0 25px 45px rgba(0, 0, 0, 0.1)',
+                    boxShadow: '0 25px 45px rgba(0, 0, 0, 0.1)',
                 }}
                 distanceFactor={1.5}
                 position={[1.5, -1.5, 1]}
